550e8efab57eb115315ef91cb3adbcf59c723001
Adam Rizzo <arizzo.dev@gmail.com>
2021-01-26 18:53:34 -0500

    added buttons to enemy table

--- creatureCreation.js
+++ creatureCreation.js
@@ -44,3 +44,30 @@ creatureList.push(grizzly);
 function printCreatureList() {
   console.log(creatureList);
 }
+
+function fillCreatureTable() {
+  document.getElementById("c1Name").innerHTML = creatureList[0].name;
+  document.getElementById("c2Name").innerHTML = creatureList[1].name;
+  document.getElementById("c3Name").innerHTML = creatureList[2].name;
+  document.getElementById("c4Name").innerHTML = creatureList[3].name;
+  document.getElementById("c1Level").innerHTML = creatureList[0].level;
+  document.getElementById("c2Level").innerHTML = creatureList[1].level;
+  document.getElementById("c3Level").innerHTML = creatureList[2].level;
+  document.getElementById("c4Level").innerHTML = creatureList[3].level;
+  document.getElementById("c1Strength").innerHTML = creatureList[0].strength;
+  document.getElementById("c2Strength").innerHTML = creatureList[1].strength;
+  document.getElementById("c3Strength").innerHTML = creatureList[2].strength;
+  document.getElementById("c4Strength").innerHTML = creatureList[3].strength;
+  document.getElementById("c1Speed").innerHTML = creatureList[0].speed;
+  document.getElementById("c2Speed").innerHTML = creatureList[1].speed;
+  document.getElementById("c3Speed").innerHTML = creatureList[2].speed;
+  document.getElementById("c4Speed").innerHTML = creatureList[3].speed;
+  document.getElementById("c1Armor").innerHTML = creatureList[0].armor;
+  document.getElementById("c2Armor").innerHTML = creatureList[1].armor;
+  document.getElementById("c3Armor").innerHTML = creatureList[2].armor;
+  document.getElementById("c4Armor").innerHTML = creatureList[3].armor;
+  document.getElementById("c1HP").innerHTML = creatureList[0].hp;
+  document.getElementById("c2HP").innerHTML = creatureList[1].hp;
+  document.getElementById("c3HP").innerHTML = creatureList[2].hp;
+  document.getElementById("c4HP").innerHTML = creatureList[3].hp;
+}
